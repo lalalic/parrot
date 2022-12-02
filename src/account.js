2e0f4aa415ae2ef117fc2a2eeb435d136f88e3ca
lalalic <lalalic@139.com>
2022-12-02 12:13:02 -0500

    section settings

--- src/account.js
+++ src/account.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {View, Text, Pressable, FlatList} from "react-native"
+import {View, Text, Pressable, SectionList} from "react-native"
 import { Link } from "react-router-native"
 import { MaterialIcons } from '@expo/vector-icons';
 import { useDispatch } from "react-redux";
@@ -9,36 +9,38 @@ export default ()=>{
     const dispatch=useDispatch()
     return (
         <View style={{flex: 1,padding:4, paddingTop:20}}>
-            <FlatList keyExtractor={a=>a.name}
-                renderItem={({index:i,item:{name,icon, href=`/account/${name}`}})=>(
-                    <Link to={href}>
-                        <View style={{flexDirection:"row",width:"100%",height:50, paddingTop:5,paddingBottom:5, borderBottomWidth:1,borderColor:"gray",borderTopWidth:!i&&1}}>
+            <SectionList 
+                keyExtractor={a=>a.name}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={{flex:1, fontSize:16, paddingTop:20, paddingLeft: 10}}>{title}</Text>
+                )}
+                renderItem={({index:i,item:{name,icon, href=`/account/${name}`, onPress}})=>{
+                    const content=(
+                        <View style={{flexDirection:"row",width:"100%",height:50, paddingTop:5,paddingBottom:5, borderBottomWidth:1,borderColor:"gray"}}>
                             <MaterialIcons style={{paddingTop:5}} name={icon} size={30} />
                             <Text style={{flexGrow:1,marginLeft:4,paddingTop:12}}>{name}</Text>
-                            <MaterialIcons style={{paddingTop:8}} name="keyboard-arrow-right"   />
+                            {!onPress && <MaterialIcons style={{paddingTop:8}} name="keyboard-arrow-right"   />}
                         </View>
-                    </Link>
-                )} 
-                data={[
-                    {name:"Policy", icon:"policy"},
-                    {name:"Files", icon:"file-present"},
-                    {name:"Artest", icon:"person-pin-circle"},
-                ]} />     
-            
-            <Pressable 
-                onPress={e=>{
-                    dispatch(Ted.util.resetApiState())
-                    /*
-                    dispatch({type:"talk/clear/all"})
-                    dispatch({type:"audiobook/clear"})
-                    dispatch({type:"picturebook/clear"})
-                    */
-                }}
-                style={{flexDirection:"row",width:"100%",height:50, paddingTop:5,paddingBottom:5, borderBottomWidth:1,borderColor:"gray",borderTopWidth:1}}>
-                <MaterialIcons style={{paddingTop:5}} name={"cleaning-services"} size={30} />
-                <Text style={{flexGrow:1,marginLeft:4,paddingTop:12}}>Clear Cache</Text>
-                <MaterialIcons style={{paddingTop:8}} name="keyboard-arrow-right"   />
-            </Pressable>
+                    )
+                    return onPress ? 
+                        (<Pressable {...{children:content, onPress}}/>)
+                        :(<Link to={href} children={content}/>)
+                }} 
+                sections={[
+                    {title:"Settings", data:[
+                        {name:"Policy", icon:"policy"},
+                    ]},
+                    {title:"Developer", data:[
+                        {name:"Files", icon:"file-present"},
+                        {name:"Artest", icon:"person-pin-circle"},
+                    ]},
+                    {title:"Cache", data:[
+                        {name:"Clear Ted", icon: "cleaning-services", onPress:e=>dispatch(Ted.util.resetApiState())},
+                        {name:"Clear Talk", icon: "cleaning-services", onPress:e=>dispatch({type:"talk/clear/all"})},
+                        {name:"Clear Audio Book", icon: "cleaning-services", onPress:e=>dispatch({type:"audiobook/clear"})},
+                        {name:"Clear Picture Book", icon: "cleaning-services", onPress:e=>dispatch({type:"picturebook/clear"})},
+                    ]}
+                ]} /> 
         </View>
     )
 }
