352a7cb412a2eb56afa48370f657d444c2e8401d
lalalic <lalalic@139.com>
2023-07-16 15:51:04 -0400

    debug

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import useQili from "react-native-use-qili"
 
 useQili({
     apiKey:"parrot",
+    api: __DEV__ ? "http://localhost:9080/1/graphql" : undefined,
     bridge:{
         accessToken:""
     }
